9d923f3882f791a103cfd66dc0c4448082b4c509
tanay2706 <99145888+tanay2706@users.noreply.github.com>
2022-02-08 18:07:39 +0000

    Add files via upload

---
+++ class10a/class10a/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+  var  height = [20,10,70,80,40]
+  var sum = height[0] +  height[1] +  height[2] +  height[3] +  height[4] 
+  var average = sum/height.length;
+  console.log(average);
+}                                                                                
+
+function draw() 
+{
+background(51);
+
+}
+
